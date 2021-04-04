bc32dcde101d3b5269418b9fc2bff07409729d04
bhuvi366 <76648389+bhuvi366@users.noreply.github.com>
2021-04-04 17:41:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var fObject,mObject;
+function setup() {
+  createCanvas(800,400);
+  fObject=createSprite(400, 200, 50, 80);
+  mObject=createSprite(400, 200, 80, 50);
+  fObject.shapeColor="red";
+  mObject.shapeColor="red";
+
+  
+}
+
+function draw() {
+  background(255,255,255);
+  mObject.x=mouseX  
+  mObject.y=mouseY 
+  console.log("distance betweenCentre "+(fObject.width/2+mObject.width/2));
+  console.log("distance betweenObject "+(fObject.x-mObject.x));
+  if(fObject.width/2+mObject.width/2===fObject.x-mObject.x){
+    fObject.shapeColor="blue";
+  mObject.shapeColor="blue";
+  }
+  else{
+    fObject.shapeColor="red";
+  mObject.shapeColor="red";
+  }
+  drawSprites();
+}
